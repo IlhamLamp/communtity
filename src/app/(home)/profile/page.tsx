09452c6c287fa7ca6201f933026c995aaa4d884d
IlhamLamp <ilhamutomo24@gmail.com>
2024-10-13 23:35:01 +0700

    test: profile

--- src/app/(home)/profile/page.tsx
+++ src/app/(home)/profile/page.tsx
@@ -1,7 +1,9 @@
 import ContentProfile from "@/components/Content/profile";
 import { profileData } from "@/components/Content/profile/data";
+import { useAuth } from "@/context/AuthContext";
 
 const ProfilePage: React.FC = () => {
+  const { isLogin, isLoading } = useAuth();
   return (
     <div className="bg-gray-100">
       <ContentProfile key={profileData?.data._id} data={profileData.data} />
