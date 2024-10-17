8a124a992701854e2da8b5342a66269159a916e9
IlhamLamp <ilhamutomo24@gmail.com>
2024-10-17 21:29:14 +0700

    fix: null checking social gorup btn

--- src/components/Buttons/Social/SocialProfileGroup.tsx
+++ src/components/Buttons/Social/SocialProfileGroup.tsx
@@ -1,15 +1,20 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SocialProfileGroup: React.FC<{ data: any }> = ({ data }) => {
   return (
     <div className="px-2 flex gap-4 rounded-sm">
-      { data.filter((link: any) => link?.is_exist).map((social: any) => (
+      {data
+        ?.filter((link: any) => link?.is_exist)
+        .map((social: any) => (
           <a key={social?.id} href={social?.link} className="cursor-pointer">
-            <FontAwesomeIcon icon={social?.icon} className="text-lg lg:text-xl text-slate-600 hover:text-navy"/>
+            <FontAwesomeIcon
+              icon={social?.icon}
+              className="text-lg lg:text-xl text-slate-600 hover:text-navy"
+            />
           </a>
-      ))}
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default SocialProfileGroup
+export default SocialProfileGroup;
