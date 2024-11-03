665d6c9ea2ce7792ebd4b66cbd8d6e73569fc2db
IlhamLamp <ilhamutomo24@gmail.com>
2024-11-03 18:29:48 +0700

    add: oauth signup btn

--- src/components/Form/RegisterAccountForm.tsx
+++ src/components/Form/RegisterAccountForm.tsx
@@ -7,6 +7,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import OauthLoginBtn from "../Buttons/OauthLoginBtn";
 
 const RegisterAccountForm: React.FC<{
   data: TRegisterUser;
@@ -180,30 +181,7 @@ const RegisterAccountForm: React.FC<{
           {isSubmitting ? "Registering..." : "Register Account"}
         </button>
       </div>
-      <div className="flex flex-col mx-auto items-center gap-4 mb-4">
-        <p className="text-sm text-gray-700 ">or, sign up with</p>
-        <div className="flex justify-center items-center space-x-4">
-          <button className="flex items-center space-x-2 bg-white shadow-md border border-gray-100 rounded-full px-4 py-2">
-            <img
-              src="https://tailus.io/sources/blocks/social/preview/images/google.svg"
-              className="h-5 w-5"
-              alt="google logo"
-            />
-            <span className="text-sm text-gray-700 ">Google</span>
-          </button>
-          <button className="flex items-center space-x-2 bg-white shadow-md border border-gray-100 rounded-full px-4 py-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              className="h-5 w-5"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-            </svg>
-            <span className="text-sm text-gray-700 ">GitHub</span>
-          </button>
-        </div>
-      </div>
+      <OauthLoginBtn />
     </form>
   );
 };
